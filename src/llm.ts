624a393a126b436cf69e5776fc5a91745890bba3
sravan <kilarusravankumar@gmail.com>
2025-08-12 18:53:52 -0500

    Updated Gemini LLM

--- src/llm.ts
+++ src/llm.ts
@@ -1,12 +1,17 @@
 import type { AIMessage } from "../types";
 import { openai } from "./ai";
+import { zodFunction } from "openai/helpers/zod";
 
-export const runLLM = async ({ messages }: { messages: AIMessage[] }) => {
+export const runLLM = async ({ messages , tools }: { messages: AIMessage[] , tools: any[]}) => {
+    const formattedTools = tools.map(zodFunction)
     const response = await openai.chat.completions.create({
-        model: 'gemini-2.0-flash-lite',
+        model: 'gemini-2.5-flash-lite',
         temperature: 0.1,
         messages,
+        tools: formattedTools,
+        tool_choice: "auto",
+        parallel_tool_calls: false,
     })
 
-    return response.choices[0].message.content
+    return response.choices[0].message
 }
