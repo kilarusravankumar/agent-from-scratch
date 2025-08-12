7c556950fb09d6ff77c353cd4383f11be48d338f
sravan <kilarusravankumar@gmail.com>
2025-08-12 18:52:35 -0500

    added sample tools

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import "dotenv/config";
 import { runLLM } from "./src/llm";
 import dotenv from "dotenv";
 import { addMessages,getMessages } from "./src/memory";
+import {z} from "zod";
+import { runAgent } from "./src/agent";
 
 dotenv.config();
 const userMessage = process.argv[2]
@@ -11,14 +13,32 @@ if (!userMessage) {
   process.exit(1)
 }
 
-await addMessages([{ role: 'user', content: userMessage }])
-const messages = await getMessages()
 
-const response = await runLLM({
-  messages,
-})
+const weatherTool = {
+  name : "get_weather",
+  description: "when weather is asked about certain location , use locationTool to get lat and long and then use this tool.",
+  parameters: z.object({
+    reasoning: z.string().describe("Why did you pick this tool?"),
+  }),
+}
 
-await addMessages([{ role: 'assistant', content: response }])
+const  locationTool = {
+  name: "get_location",
+  description: "use this get location's lat and long, and then pass it to weatherTool to get weather",
+  parameters: z.object({
+    reasoning: z.string().describe("Why did you pick this tool?"),
+  }),
+}
 
+const carTool = {
+  name: "get_carInformation",
+  description: "use this tool to get car information",
+  parameters: z.object({
+    reasoning: z.string().describe("Why did you pick this tool?"),
+  }),
+} 
+
+
+const response = await runAgent({userMessage, tools:[weatherTool, locationTool, carTool]})
 console.log(response)
 
